47190a1b69e97d3ebecffe850b9d7ebe2d35313f
yashjangir04 <jangiryash04@gmail.com>
2025-05-17 21:54:08 +0530

    Changes Made

--- src/assets/Home.jsx
+++ src/assets/Home.jsx
@@ -8,30 +8,31 @@ const Home = () => {
       {/* <img src={img1} alt="backdrop" className='w-full fixed top-10 z-0'/> */}
       <div className="w-full gap-20 md:h-screen flex flex-col md:py-0 py-25 md:flex-row px-8 md:px-24 justify-between items-center md:gap-6 mt-10 bg-[#00000089] backdrop-blur-2xl">
         <div className="home-left flex flex-col justify-center border-0 border-zinc-700 w-full text-center md:text-left">
-          <h1 className="text-white neue text-4xl md:text-7xl ">Hey there, I'm</h1>
+          <h1 className="text-white neue text-4xl md:text-7xl ">Hey there, I'm a</h1>
           <h1 className="text-white neue text-2xl md:text-6xl text-nowrap mt-6">
             {" "}
             <span className="text-purple-600 dm-sans">
               <Typewriter
                 words={[
+                  "<Full Stack Developer/>",
                   "<Competitive Programmer/>",
-                  "<Web Developer/>",
+                  "<Software Engineer/>"
                 ]}
-                loop={0}
+                loop={1}
                 cursor
                 cursorStyle="|"
                 typeSpeed={100}
                 deleteSpeed={40}
-                delaySpeed={1000}
+                delaySpeed={400}
               />
             </span>
           </h1>
           <div className="home-btns flex flex-row gap-3 mt-8 justify-center">
-            <button className="bg-purple-600 text-white hover:text-white border-2 border-transparent hover:border-2 hover:border-purple-600 px-3 py-2 text-sm md:text-md rounded-3xl duration-300 hover:bg-transparent hover:scale-105 inter cursor-pointer ">
+            <a href="./Resume.pdf" target="_blank"><button className="bg-purple-600 text-white hover:text-white border-2 border-transparent hover:border-2 hover:border-purple-600 px-3 py-2 text-sm md:text-md rounded-3xl duration-300 hover:bg-transparent hover:scale-105 inter cursor-pointer ">
               Download CV
-            </button>
+            </button></a>
             <button className="text-white border-2 border-white px-3 py-2 text-sm md:text-md rounded-3xl hover:bg-white hover:scale-105 hover:border-2 hover:border-white hover:text-black duration-300 transition inter cursor-pointer ">
-              Connect
+              <a href="https://www.linkedin.com/in/deveshjangir/" target="_blank">Connect</a>
             </button>
           </div>
         </div>
