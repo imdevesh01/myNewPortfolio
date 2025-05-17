415cd0201fa24bf762dd8a8f0a75967d885a97d1
Devesh Jangir <connect.to.devesh@gmail.com>
2025-05-17 23:58:16 +0530

    Cropped picture

--- src/assets/Home.jsx
+++ src/assets/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import img1 from "./Images/dudai.jpg";
+import img1 from "./Images/devesh.jpg";
 import { Typewriter } from "react-simple-typewriter";
 
 const Home = () => {
